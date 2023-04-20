43880019c5141518b6b39809c6651669d360433b
Andreas Loizides <andreas16700@icloud.com>
2023-04-20 16:47:28 +0300

    add methods for bulk variant updates, inventory getter and updates

--- Sources/ShopifyKit/ClientProtocol.swift
+++ Sources/ShopifyKit/ClientProtocol.swift
@@ -6,12 +6,12 @@
 //
 
 import Foundation
-import ShopifyKit
 
 public protocol ShopifyClientProtocol{
 	//MARK: Variant
 	func deleteVariant(ofProductID productID: Int, variantID: Int)async->Bool
 	func updateVariant(with update: SHVariantUpdate)async -> SHVariant?
+	func updateVariants(with updates: [SHVariantUpdate])async -> SHVariant?
 	func createNewVariant(variant: SHVariantUpdate, for productID: Int)async ->SHVariant?
 	//MARK: Product
 	func deleteProduct(id: Int)async -> Bool
@@ -23,7 +23,9 @@ public protocol ShopifyClientProtocol{
 	func getIDOfProduct(withHandle handle: String)async -> Int?
 	//MARK: Inventory
 	func updateInventory(current: InventoryLevel, update: SHInventorySet)async->InventoryLevel?
+	func updateInventories(updates: [(InventoryLevel,SHInventorySet)])async->[InventoryLevel]?
 	func getInventory(of invItemID: Int)async ->InventoryLevel?
+	func getInventories(of invItemIDs: [Int])async ->[InventoryLevel]?
 	func getAllInventories()async -> [InventoryLevel]?
 	func getAllInventories(of locationID: Int)async ->[InventoryLevel]?
 	func getAllLocations()async ->[SHLocation]?
