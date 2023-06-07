1816a87de3e8188206fa287eb768db57ae2d3270
Andreas Loizides <andreas16700@icloud.com>
2023-06-07 11:29:35 +0300

    protocol methods now should return a result

--- Sources/ShopifyKit/ClientProtocol.swift
+++ Sources/ShopifyKit/ClientProtocol.swift
@@ -7,27 +7,31 @@
 
 import Foundation
 
+public struct ShopifyError: Error{
+	let response: String?
+}
+
 public protocol ShopifyClientProtocol{
 	//MARK: Variant
-	func deleteVariant(ofProductID productID: Int, variantID: Int)async->Bool
-	func updateVariant(with update: SHVariantUpdate)async -> SHVariant?
-	func updateVariants(with updates: [SHVariantUpdate])async -> [SHVariant]?
-	func createNewVariant(variant: SHVariantUpdate, for productID: Int)async ->SHVariant?
-	func createNewViariants(variants: [SHVariantUpdate], for productID: Int)async -> [SHVariant]?
+	func deleteVariant(ofProductID productID: Int, variantID: Int)async->Result<Bool,ShopifyError>
+	func updateVariant(with update: SHVariantUpdate)async -> Result<SHVariant,ShopifyError>
+	func updateVariants(with updates: [SHVariantUpdate])async -> Result<[SHVariant],ShopifyError>
+	func createNewVariant(variant: SHVariantUpdate, for productID: Int)async ->Result<SHVariant,ShopifyError>
+	func createNewViariants(variants: [SHVariantUpdate], for productID: Int)async -> Result<[SHVariant],ShopifyError>
 	//MARK: Product
-	func deleteProduct(id: Int)async -> Bool
-	func updateProduct(with update: SHProductUpdate)async ->SHProduct?
-	func createNewProduct(new: SHProduct)async->SHProduct?
-	func getAllProducts()async->[SHProduct]?
-	func getProduct(withHandle handle: String)async -> SHProduct?
-	func getProduct(withID id: Int)async -> SHProduct?
-	func getIDOfProduct(withHandle handle: String)async -> Int?
+	func deleteProduct(id: Int)async -> Result<Bool,ShopifyError>
+	func updateProduct(with update: SHProductUpdate)async ->Result<SHProduct,ShopifyError>
+	func createNewProduct(new: SHProduct)async->Result<SHProduct,ShopifyError>
+	func getAllProducts()async->Result<[SHProduct],ShopifyError>
+	func getProduct(withHandle handle: String)async -> Result<SHProduct,ShopifyError>
+	func getProduct(withID id: Int)async -> Result<SHProduct,ShopifyError>
+	func getIDOfProduct(withHandle handle: String)async -> Result<Int,ShopifyError>
 	//MARK: Inventory
-	func updateInventory(current: InventoryLevel, update: SHInventorySet)async->InventoryLevel?
-	func updateInventories(updates: [SHInventorySet])async->[InventoryLevel]?
-	func getInventory(of invItemID: Int)async ->InventoryLevel?
-	func getInventories(of invItemIDs: [Int])async ->[InventoryLevel]?
-	func getAllInventories()async -> [InventoryLevel]?
-	func getAllInventories(of locationID: Int)async ->[InventoryLevel]?
-	func getAllLocations()async ->[SHLocation]?
+	func updateInventory(current: InventoryLevel, update: SHInventorySet)async->Result<InventoryLevel,ShopifyError>
+	func updateInventories(updates: [SHInventorySet])async->Result<[InventoryLevel],ShopifyError>
+	func getInventory(of invItemID: Int)async ->Result<InventoryLevel,ShopifyError>
+	func getInventories(of invItemIDs: [Int])async ->Result<[InventoryLevel],ShopifyError>
+	func getAllInventories()async -> Result<[InventoryLevel],ShopifyError>
+	func getAllInventories(of locationID: Int)async ->Result<[InventoryLevel],ShopifyError>
+	func getAllLocations()async ->Result<[SHLocation],ShopifyError>
 }
